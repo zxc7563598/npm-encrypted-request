3931b2dc45aa85624bf412de74f465c6929145ae
zxc7563598 <junjie.he.925@gmail.com>
2025-08-28 15:29:40 +0800

    refactor(core): 大幅重构加密逻辑，AES密钥自动生成，通过RSA传递密钥给后端
    BREAKING CHANGE: 逻辑变更，增强安全性，不对过去进行兼容，因此版本号提升到2.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,54 @@
 import { aesEncrypt } from "./encryptor.js";
 import { generateSign } from "./signer.js";
+import forge from "node-forge";
+
+/**
+ * 随机生成 16 字节 AES key/IV
+ *
+ * @returns { key: string, iv: string }
+ */
+function generateRandomKeyIv() {
+  const key = forge.random.getBytesSync(16);
+  const iv = forge.random.getBytesSync(16);
+  return { key, iv };
+}
+
+/**
+ * 用 RSA 公钥加密
+ *
+ * @param key AES Key
+ * @param iv AES IV
+ * @param pubKeyPem RSA 公钥
+ *
+ * @returns
+ */
+function rsaEncrypt(key: string, iv: string, pubKeyPem: string): string {
+  const pubKey = forge.pki.publicKeyFromPem(pubKeyPem);
+  const payload = forge.util.encode64(key) + forge.util.encode64(iv);
+  const encrypted = pubKey.encrypt(payload, "RSA-OAEP");
+  return forge.util.encode64(encrypted);
+}
 
 export interface EncryptOptions {
-  appKey: string;
-  aesKey: string;
-  aesIv: string;
-  token?: string;
+  rsaPubKey: string;
 }
 
 export function encryptRequest(
   data: object,
   options: EncryptOptions
 ): Record<string, any> {
+  const { key, iv } = generateRandomKeyIv();
   const timestamp = Math.floor(Date.now() / 1000);
-  const sign = generateSign(options.appKey, timestamp);
-  const en_data = aesEncrypt(data, options.aesKey, options.aesIv);
-  if (options.token) {
-    return {
-      timestamp,
-      sign,
-      en_data,
-      token: options.token,
-    };
-  } else {
-    return {
-      timestamp,
-      sign,
-      en_data,
-    };
-  }
+  const sign = generateSign(
+    forge.util.encode64(key) + forge.util.encode64(iv),
+    timestamp
+  );
+  const en_data = aesEncrypt(data, key, iv);
+  const enc_payload = rsaEncrypt(key, iv, options.rsaPubKey);
+  return {
+    timestamp,
+    sign,
+    en_data,
+    enc_payload,
+  };
 }
--- tests/demo.ts
+++ tests/demo.ts
@@ -1,12 +1,21 @@
 import { encryptRequest, EncryptOptions } from "../src/index.js";
+
+const pubKey = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvsu8OaVsMQ6bYZBvN8tN
+swhxRB5giPfMH7es3VqUVBZe/mBivLRGrNLr0FQx4HclfLpw96zh68FXMBfo8ZjF
+vBQxdfPlvRUiKfmXAGHDgv6pQBSnSE+WNBDgYEqOA9qYYpQSgNqR5vib76dkpwry
+ci43/RqKdSa68RD694jumdM5Uv1ZZIsboEn3rq+FhPwnz1c/0d6J/SroXwHQZpKf
+muYmbLtd8MKtIuLHXOX5Uq3AzECEydGcIJnlMhsbpzrStXdSLl+FYXuGHjbSo9x+
+Fir3bpaB/+0DJaB63/7R6hI/WVnRXv0HI+Pu1+VgSyiTxOriL85tkt+1KdXZdvqL
+xwIDAQAB
+-----END PUBLIC KEY-----`;
+
 const options: EncryptOptions = {
-  appKey: "JzmiM5jHFlG8qdjb8nSKK58NmSLS2h4Z",
-  aesKey: "hpTIDOsgBTJkBDol",
-  aesIv: "5EMdiDp6AjwAmUoe",
-  token: "tokentokentokentokentokentokentoken",
+  rsaPubKey: pubKey,
 };
 
 const data = { message: "Hello World" };
 
-const encrypted = encryptRequest(data, options);
-console.log("Encrypted request:", encrypted);
+const payload = encryptRequest(data, options);
+
+console.log(payload);
